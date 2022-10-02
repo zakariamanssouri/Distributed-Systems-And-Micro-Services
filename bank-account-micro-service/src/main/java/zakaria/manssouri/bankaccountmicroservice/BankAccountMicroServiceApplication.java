92da8c0a83f42ab72f613f8682c463c820321330
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-02 15:29:38 +0100

    add instructions for creating a 10 accounts for testing

--- bank-account-micro-service/src/main/java/zakaria/manssouri/bankaccountmicroservice/BankAccountMicroServiceApplication.java
+++ bank-account-micro-service/src/main/java/zakaria/manssouri/bankaccountmicroservice/BankAccountMicroServiceApplication.java
@@ -1,13 +1,38 @@
 package zakaria.manssouri.bankaccountmicroservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import zakaria.manssouri.bankaccountmicroservice.entities.BankAccount;
+import zakaria.manssouri.bankaccountmicroservice.enums.AccountType;
+import zakaria.manssouri.bankaccountmicroservice.repositories.BankAccountRepository;
+
+import java.util.Date;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BankAccountMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankAccountMicroServiceApplication.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner start(BankAccountRepository bankAccountRepository){
+		return args -> {
+			for (int i = 0; i < 10; i++) {
+				BankAccount bankAccount = BankAccount.builder()
+						.id(UUID.randomUUID().toString())
+						.type(Math.random() > 0.5 ? AccountType.CURRENT_ACCOUNT : AccountType.SAVING_ACCOUNT)
+						.balance(10000 + Math.random() * 90000)
+						.createdAt(new Date())
+						.currency("MAD")
+						.build();
+				bankAccountRepository.save(bankAccount);
+			}
+		};
 	}
 
 }
