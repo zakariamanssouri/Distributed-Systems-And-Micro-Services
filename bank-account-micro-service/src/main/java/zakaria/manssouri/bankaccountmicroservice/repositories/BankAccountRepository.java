ca825cc2836db8d0a65ffd9b377b795b478cdb56
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-02 17:52:17 +0100

    add a findByType method and  RepositoryRestRessource annotation

--- bank-account-micro-service/src/main/java/zakaria/manssouri/bankaccountmicroservice/repositories/BankAccountRepository.java
+++ bank-account-micro-service/src/main/java/zakaria/manssouri/bankaccountmicroservice/repositories/BankAccountRepository.java
@@ -1,8 +1,17 @@
 package zakaria.manssouri.bankaccountmicroservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import zakaria.manssouri.bankaccountmicroservice.entities.BankAccount;
+import zakaria.manssouri.bankaccountmicroservice.enums.AccountType;
 
+import javax.websocket.server.PathParam;
+import java.util.List;
+
+@RepositoryRestResource // generate get post delete put methods for manipulating BankAccount
 public interface BankAccountRepository extends JpaRepository<BankAccount,String > {
-
+    @RestResource(path = "/byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
 }
