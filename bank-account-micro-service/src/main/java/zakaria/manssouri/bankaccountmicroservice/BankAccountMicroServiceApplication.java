b00361281c27d1e4d7a8e4da546a382c1fb6d841
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-02 14:55:58 +0100

    initialize bank account micro service project

---
+++ bank-account-micro-service/src/main/java/zakaria/manssouri/bankaccountmicroservice/BankAccountMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package zakaria.manssouri.bankaccountmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccountMicroServiceApplication.class, args);
+	}
+
+}
